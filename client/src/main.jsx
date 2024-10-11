4d3d5b8f5b49582a637509421ceaab3e0052978b
santosh302 <santoshkumarsy05@gmail.com>
2024-10-11 13:38:48 +0530

    frotend dependencies install successfully

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {Toaster} from 'react-hot-toast'
+import {Provider} from 'react-redux'
+import {BrowserRouter} from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
 import App from './App.jsx'
+import store from './redux/store.js'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store= {store}>
+    <BrowserRouter>
     <App />
-  </StrictMode>,
+    <Toaster/>
+    </BrowserRouter>
+  </Provider>
 )
---
+++ client/src/redux/slices/authSlice.jsx
@@ -0,0 +1,3 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit"
+import toast from "react-hot-toast";
+
